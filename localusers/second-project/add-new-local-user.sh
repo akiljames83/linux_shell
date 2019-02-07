1a34f7bfe68b662963a2b0ee77db742808a413ad
Akil Hamilton <akil.james83@gmail.com>
2019-02-07 07:26:36 -0500

    Edits to project2

--- localusers/second-project/add-new-local-user.sh
+++ localusers/second-project/add-new-local-user.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+#
+# This script creates a new user on the local system.
+# You must supply a username as an argument to the script.
+# Optionally, you can also provide a comment for the account as an argument
 
 # Ensure script is executed by root
 if [[ "${UID}" -ne 0 ]]
 then
-  echo "Script can only be run by sudo"
+  echo "Script can only be run as root."
   exit 1
 fi
 
 # Checks if command line has greater than 1 argument
 if [[ "${#}" = 0 ]]
 then
-  echo "Usage: USER_NAME [COMMENT]..."
+  echo "Usage `basename ${0}`: USER_NAME [COMMENT]..."
   exit 1
 fi
 
