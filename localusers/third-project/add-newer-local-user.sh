33c2b3ad1344cc9c82ee5a74748d403226e5590c
Akil Hamilton <akil.james83@gmail.com>
2019-02-08 18:14:25 -0500

    More edits to third project

--- localusers/third-project/add-newer-local-user.sh
+++ localusers/third-project/add-newer-local-user.sh
@@ -3,14 +3,14 @@
 # Ensure that the user is root for them to run the script
 if [[ "${UID}" -ne 0 ]]
 then
-  echo "Must be root to access this program."
+  echo "Must be root to access this program." >&2 # send to STDERR
   exit 1;
 fi
 
 # Ensure the correct amount of command line arguments are provided
 if [[ "$#" = 0 ]]
 then
-  echo "Usage: USER_NAME [COMMENT]..."
+  echo "Usage: USER_NAME [COMMENT]..." >&2
   exit 1;
 fi
 
@@ -25,22 +25,22 @@ R_CHAR=`echo $S | fold -w1 | shuf | head -c1`
 PASSWORD=`date +%s%N${RANDOM}${R_CHAR}${RANDOM} | sha256sum | head -c32`
 
 # Creates the new user
-useradd -m $USERNAME -c "$COMMENT"
+useradd -m $USERNAME -c "$COMMENT" &> /dev/null
 
 # Check to see if the user was not able to be created
 if [[ "$?" -ne 0 ]]
 then
-  echo "User was not able to be created."
+  echo "User was not able to be created." >&2
   exit 1;
 fi
 
 # Add password to the user
-echo "${PASSWORD}${R_CHAR}" | passwd --stdin $USERNAME
+echo "${PASSWORD}${R_CHAR}" | passwd --stdin $USERNAME &> /dev/null
 
 # Check if there was an error in creating the users password
 if [[ "$?" -ne 0 ]]
 then
-  echo "Could not set user password."
+  echo "Could not set user password." >&2
   exit 1;
 fi
 
