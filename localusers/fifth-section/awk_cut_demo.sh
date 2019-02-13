f9c3d6551ee974d70073d17c54e896c5c1f2d2b7
Akil Hamilton <akil.james83@gmail.com>
2019-02-12 23:44:35 -0500

    Clean up port script

--- localusers/fifth-section/awk_cut_demo.sh
+++ localusers/fifth-section/awk_cut_demo.sh
@@ -5,9 +5,20 @@
 # command.
 
 netstat -nutl | grep -E "^Active|^Prot"
+echo
+netstat -nutl | grep -vE "^Active|^Prot"
 
 # -E for extended regular expressions with grep
 # -v to negate the pattern that is searched for; i.e. NOT this pattern
 # | the or operator for a regular expression statement
 # ^ The anchor for the front of a line to check if pattern exists
+echo
+echo "Filtered for ports:"
+netstat -4nutl | grep -vE "^A|^P" | awk '{print $4}' | cut -d ':' -f 2
+# Processing:
+# - Nestat for data
+# - grep to take out headers
+# - awk to select fourth coloumn
+# - cut to seperate at the colon
+echo
 
